44885a89d62b0aa17a94fede1732add45671dc84
Satoshi Kataoka <k.3104is@gmail.com>
2023-02-01 21:48:56 +0900

    update

--- install_ssd.sh
+++ install_ssd.sh
@@ -1,26 +1,36 @@
 #!/bin/bash
 
+# you should execute bellow command.
+# `sudo bash ./install_ssd.sh`
+
 set -x
 
 SSD_DEV="/dev/sdb"
 MOUNT_POINT="/mnt/ssd"
 
-USERS=`ls /home`
+USERS=(`ls /home`)
 
 # clear ssd
 dd if=/dev/zero of=${SSD_DEV}
 
 # create parition
 parted -a optimal ${SSD_DEV} -s mklabel msdos \
-  -s mkpart primary ext4 
+  -s mkpart primary ext4 2048 100% 
 fdisk -l ${SSD_DEV}
 
 
 mkdir -p ${MOUNT_POINT}
+mkfs -t ext4 ${SSD_DEV}1
 mount -t ext4 ${SSD_DEV}1 ${MOUNT_POINT}
+
 pushd ${MOUNT_POINT}
 for dir in ${USERS[@]}
 do
   mkdir ${dir}
-  chown ${dir}:${dir} ${dir}
+  chown -R ${dir}:${dir} ${dir}
+done
 popd
+
+# config auto
+echo "${SSD_DEV}1 ${MOUNT_POINT} auto defaults 0 0" >> /etc/fstab
+
